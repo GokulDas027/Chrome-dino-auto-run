6f2372c8e1478b25e95bd4664f956b3f54f0c6ab
GokulDas027 <gokuldas027@gmail.com>
2019-08-12 19:31:18 +0530

    fix: background change panic

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import ctypes
 
 # bbox = (450, 230, 500, 265)
 # bbox = (Left, Top, Right, Bottom)
-bbox = (865, 240, 940, 275)  
-prev_frame = np.zeros((bbox[3]-bbox[1], bbox[2]-bbox[0], 3))
+bbox = (865, 240, 950, 275)
+shape = (bbox[3]-bbox[1], bbox[2]-bbox[0], 3)
+bg = 255
+ref_frame = np.full(shape, bg)
 
 while True:
     img = ImageGrab.grab(bbox)
@@ -15,7 +17,12 @@ while True:
     # frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame", frame) 
     
-    frame_diff = np.subtract(prev_frame, frame).sum()
+    if bg != frame[0][0][0]:
+        bg = frame[0][0][0]
+        ref_frame = np.full(shape, bg)
+
+    frame_diff = np.subtract(ref_frame, frame).sum()
+
     if frame_diff != 0:
         ctypes.windll.user32.keybd_event(0x20, 0, 0, 0)  # Space is down
         print(frame_diff)
